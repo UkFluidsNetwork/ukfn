0cf50581fd75439a6a2c8bcb59eeadddbf2b4616
Javier Arias <ja573@newton.ac.uk>
2016-08-21 16:19:09 +0100

    foreign key to addresses in institutions

--- database/migrations/2016_08_14_000009_create_institutions_table.php
+++ database/migrations/2016_08_14_000009_create_institutions_table.php
@@ -15,11 +15,13 @@ class CreateInstitutionsTable extends Migration
         Schema::create('institutions', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('institutiontype_id')->unsigned();
+            $table->integer('address_id')->unsigned();
             $table->timestamps();
             $table->string('name', 255);
             $table->boolean('deleted');
             
             $table->foreign('institutiontype_id')->references('id')->on('institutiontypes');
+            $table->foreign('address_id')->references('id')->on('addresses');
         });
     }
 
