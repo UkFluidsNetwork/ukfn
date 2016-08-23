6824989d2be78c30d6fccd7085ef2881940281e7
Javier Arias <ja573@newton.ac.uk>
2016-08-23 11:33:23 +0100

    populate events and news

--- database/migrations/2016_08_22_081713_create_news_table.php
+++ database/migrations/2016_08_22_081713_create_news_table.php
@@ -12,15 +12,34 @@ class CreateNewsTable extends Migration
      */
     public function up()
     {
-        Schema::create('news', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('title', 100);
-            $table->longtext('description')->nullable();
-            $table->integer('user_id')->unsigned();
-            $table->timestamps();
-            
-            $table->foreign('user_id')->references('id')->on('users');
-        });
+      Schema::create('news', function (Blueprint $table) {
+        $table->increments('id');
+        $table->string('title', 100);
+        $table->longtext('description')->nullable();
+        $table->integer('user_id')->unsigned();
+        $table->timestamps();
+
+        $table->foreign('user_id')->references('id')->on('users');
+      });
+      // populate
+      DB::table('news')->insert(
+        [
+          [
+            'title' => 'SIG',
+            'description' => 'First call for proposals for Special Interest Groups (SIGs) <br><a href="/sig" class="red-link">View Page</a>',
+            'user_id' => 1,
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+          ],
+          [
+            'title' => 'SRV',
+            'description' => 'Rolling call for proposals for Short Research Visits (SRVs) <br><a href="/srv" class="red-link">View Page</a>',
+            'user_id' => 1,
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+          ],
+        ]
+      );
     }
 
     /**
--- database/migrations/2016_08_22_081722_create_events_table.php
+++ database/migrations/2016_08_22_081722_create_events_table.php
@@ -12,17 +12,31 @@ class CreateEventsTable extends Migration
      */
     public function up()
     {
-        Schema::create('events', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('title', 100);
-            $table->string('subtitle', 100)->nullable();
-            $table->longtext('description')->nullable();
-            $table->dateTime('start')->nullable();
-            $table->integer('user_id')->unsigned();
-            $table->timestamps();
-            
-            $table->foreign('user_id')->references('id')->on('users');
-        });
+      Schema::create('events', function (Blueprint $table) {
+        $table->increments('id');
+        $table->string('title', 100);
+        $table->string('subtitle', 100)->nullable();
+        $table->longtext('description')->nullable();
+        $table->dateTime('start')->nullable();
+        $table->integer('user_id')->unsigned();
+        $table->timestamps();
+
+        $table->foreign('user_id')->references('id')->on('users');
+      });
+      // populate
+      DB::table('events')->insert(
+        [
+          [
+            'title' => 'UKFN launch event',
+            'subtitle' => 'ICL',
+            'description' => 'During the UK Fluids Conference',
+            'start' => '2016-09-09 13:30:00',
+            'user_id' => 1,
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+          ]
+        ]
+      );
     }
 
     /**
