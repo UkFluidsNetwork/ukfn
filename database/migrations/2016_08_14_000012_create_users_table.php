1ed8ac4a6d68f4394ca9b2cd5b0dd82e4af5efb4
Javier Arias <ja573@newton.ac.uk>
2016-08-23 11:18:38 +0100

    populate users

--- database/migrations/2016_08_14_000012_create_users_table.php
+++ database/migrations/2016_08_14_000012_create_users_table.php
@@ -12,22 +12,47 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('surname');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->integer('title_id')->unsigned();
-            $table->integer('group_id')->unsigned();
-            $table->integer('department_id')->unsigned()->nullable();
-            $table->rememberToken();
-            $table->timestamps();
-            
-            $table->foreign('title_id')->references('id')->on('titles');
-            $table->foreign('group_id')->references('id')->on('groups');
-            $table->foreign('department_id')->references('id')->on('departments');
-        });
+      Schema::create('users', function (Blueprint $table) {
+        $table->increments('id');
+        $table->string('name');
+        $table->string('surname');
+        $table->string('email')->unique();
+        $table->string('password');
+        $table->integer('title_id')->unsigned();
+        $table->integer('group_id')->unsigned();
+        $table->integer('department_id')->unsigned()->nullable();
+        $table->rememberToken();
+        $table->timestamps();
+
+        $table->foreign('title_id')->references('id')->on('titles');
+        $table->foreign('group_id')->references('id')->on('groups');
+        $table->foreign('department_id')->references('id')->on('departments');
+      });
+      // populate
+      DB::table('users')->insert(
+        [
+          [
+            'name' => 'Javier',
+            'surname' => 'Arias',
+            'email' => 'ja573@cam.ac.uk',
+            'password' => bcrypt("password"),
+            'title_id' => 3,
+            'group_id' => 1,
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+          ],
+          [
+            'name' => 'Robert',
+            'surname' => 'Barczyk',
+            'email' => 'robert@barczyk.net',
+            'password' => bcrypt("password"),
+            'title_id' => 3,
+            'group_id' => 1,
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+          ],
+        ]
+      );
     }
 
     /**
