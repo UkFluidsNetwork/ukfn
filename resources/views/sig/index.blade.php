1c8b6b81f74f56452f496c2695896abeaad13047
Javier Arias <ja573@newton.ac.uk>
2016-08-23 12:04:52 +0100

    jumbotron in sig and srv

--- resources/views/sig/index.blade.php
+++ resources/views/sig/index.blade.php
@@ -1,15 +1,17 @@
 @extends('layouts.master')
 @section('content')
 
-          <h2>SIG</h2>
-          <p>
-            The UKFN is pleased to invite proposals for the first round of Special Interest Groups. 
-          </p>
-          <p>
-            This note gives the context of the call and sets out the information to provide in a proposal.
-          </p>
-          <p>
-            The closing date is 31st Oct 2016. <a href="{{ asset('files/UKFN_SIGs_call_for_proposals.pdf') }}">[UKFN_SIGs_call_for_proposals.pdf]</a>
-          </p>
+  <div class="jumbotron">
+    <h2>SIG</h2>
+    <p>
+      The UKFN is pleased to invite proposals for the first round of Special Interest Groups. 
+    </p>
+    <p>
+      This note gives the context of the call and sets out the information to provide in a proposal.
+    </p>
+    <p>
+      The closing date is 31st Oct 2016. <a href="{{ asset('files/UKFN_SIGs_call_for_proposals.pdf') }}">[UKFN_SIGs_call_for_proposals.pdf]</a>
+    </p>
+  </div>
          
 @endsection
--- resources/views/srv/index.blade.php
+++ resources/views/srv/index.blade.php
@@ -1,15 +1,17 @@
 @extends('layouts.master')
 @section('content')
 
-          <h2>SRV</h2>
-          <p>
-            The UKFN is pleased to invite proposals for SRVs. This note gives the context of the call and sets out the information you should provide in your proposal. 
-          <p>
-          <p>
-            <a href="{{ asset('files/UKFN_SRVs_call_for_proposals.pdf') }}">[UKFN_SRVs_call_for_proposals.pdf]</a>
-          </p>
-          <p>
-            There is no closing date, and proposals will be assessed on a rolling basis.
-          </p>
+  <div class="jumbotron">
+    <h2>SRV</h2>
+    <p>
+      The UKFN is pleased to invite proposals for SRVs. This note gives the context of the call and sets out the information you should provide in your proposal. 
+    <p>
+    <p>
+      <a href="{{ asset('files/UKFN_SRVs_call_for_proposals.pdf') }}">[UKFN_SRVs_call_for_proposals.pdf]</a>
+    </p>
+    <p>
+      There is no closing date, and proposals will be assessed on a rolling basis.
+    </p>
+  </div>
       
 @endsection
