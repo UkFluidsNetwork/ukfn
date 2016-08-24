ac49264dcd65c0513d484273ea862ac3208ffc9b
Robert Barczyk <rb783@cam.ac.uk>
2016-08-24 10:50:34 +0100

    CSS

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,6 +2,12 @@
 <html>
   <head>
       <title>UK Fluids Network</title>
+      <!-- For The jQuerry to work on IE 11 -->
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">      
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="apple-mobile-web-app-capable" content="yes">
+      
       <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
       <link href="{{ asset('css/main.css') }}" rel="stylesheet" type="text/css">
       <script src="{{ asset('js/jquery-2.2.4.min.js')}}"></script>
@@ -25,12 +31,6 @@
       <meta name="msapplication-TileImage" content="{{ asset('pictures/favicon/ms-icon-144x144.png') }}">
       <meta name="theme-color" content="#ffffff">
       <!-- end of favicon -->
-      
-      <!-- For The jQuerry to work on IE 11 -->
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">  
-      <meta name="apple-mobile-web-app-capable" content="yes">
-      
-      <meta name="viewport" content="width=device-width, initial-scale=1">
   </head>
   <body>
     <div class="container-fluid" id="top-content">
