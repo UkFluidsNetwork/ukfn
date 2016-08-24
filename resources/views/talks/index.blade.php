34def04a3a2b61059f8224913a16e9b76eefcc2a
Robert Barczyk <rb783@cam.ac.uk>
2016-08-24 11:33:30 +0100

    Talks View changed

--- resources/views/talks/index.blade.php
+++ resources/views/talks/index.blade.php
@@ -7,13 +7,18 @@
   
           <section class="page-header">
             <div>
-              <p class="h4 text-primary">{{ $talk->title }}</p>
-              <p>{{ $talk->series }}<p>
-              <p class="line-break-top line-break-dbl">{{ $talk->speaker }}</p>
+              <p class="h4 text-danger">{{ $talk->title }}</p>
+              <p class=""><b><i>{{ $talk->speaker }} </i></b></p>
+              <div class="text-muted">{{ \Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $talk->start)->format('l jS F') }}
+                at {{ \Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $talk->start)->format('H:i') }}</div>
+              <div class="text-muted line-break-dbl">{{ $talk->venue }}</div>
               
-              <p class="text-muted">{{ $talk->start }}</p>
-              <p>{{ $talk->venue }}</p>
-              <p>{{ $talk->abstract }}</p>
+              
+              
+              
+              
+              
+              <p class="line-break-dbl-top"> {{ $talk->abstract }}</p>
             </div>
           </section>
             
