059cd3d6cbf4cb9aa0de92f2bc8071062b9c7a74
Robert Barczyk <rb783@cam.ac.uk>
2016-08-24 10:39:58 +0100

    Mobile view fixed

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -30,6 +30,7 @@
       <meta http-equiv="X-UA-Compatible" content="IE=edge">  
       <meta name="apple-mobile-web-app-capable" content="yes">
       
+      <meta name="viewport" content="width=device-width, initial-scale=1">
   </head>
   <body>
     <div class="container-fluid" id="top-content">
