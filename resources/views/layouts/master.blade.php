548c79bfc43db5e0308d84993ad150c55fa21147
Javier Arias <ja573@newton.ac.uk>
2016-08-25 18:38:36 +0100

    updated favicon

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -22,11 +22,11 @@
       <link rel="apple-touch-icon" sizes="144x144" href="{{ asset('pictures/favicon/apple-icon-144x144.png') }}">
       <link rel="apple-touch-icon" sizes="152x152" href="{{ asset('pictures/favicon/apple-icon-152x152.png') }}">
       <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('pictures/favicon/apple-icon-180x180.png') }}">
-      <link rel="icon" type="image/png" sizes="192x192"  href="{{ asset('pictures/favicon/android-icon-192x192.png') }}">
-      <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('pictures/favicon/favicon-32x32.png') }}">
-      <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('pictures/favicon/favicon-96x96.png') }}">
-      <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('pictures/favicon/favicon-16x16.png') }}">
-      <link rel="manifest" href="/manifest.json">
+      <link rel="icon" type="image.png" sizes="192x192"  href="{{ asset('pictures/favicon/android-icon-192x192.png') }}">
+      <link rel="icon" type="image.png" sizes="32x32" href="{{ asset('pictures/favicon/favicon-32x32.png') }}">
+      <link rel="icon" type="image.png" sizes="96x96" href="{{ asset('pictures/favicon/favicon-96x96.png') }}">
+      <link rel="icon" type="image.png" sizes="16x16" href="{{ asset('pictures/favicon/favicon-16x16.png') }}">
+      <link rel="manifest" href="{{ asset('pictures/favicon/manifest.json') }}">
       <meta name="msapplication-TileColor" content="#ffffff">
       <meta name="msapplication-TileImage" content="{{ asset('pictures/favicon/ms-icon-144x144.png') }}">
       <meta name="theme-color" content="#ffffff">
