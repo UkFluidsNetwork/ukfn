6ad1aea123a35b2cb5eb8d64da73d99db9aa7290
Javier Arias <ja573@newton.ac.uk>
2016-08-31 15:45:39 +0100

    add uni of cam to venue if talk does not have it

--- app/Http/Controllers/TalksController.php
+++ app/Http/Controllers/TalksController.php
@@ -24,8 +24,12 @@ class TalksController extends Controller
       $talks[$index]['title'] = $talk->title;
       $talks[$index]['when'] = date("l jS F", strtotime($talk->start)) . " at " . date("H:i", strtotime($talk->start));
       $talks[$index]['speaker'] = $talk->speaker;
-      $talks[$index]['venue'] = $talk->venue;
       $talks[$index]['abstract'] = $talk->abstract;
+      if(!strpos(strtolower($talk->venue), "university of cambridge")) {
+        $talks[$index]['venue'] = $talk->venue .", University of Cambridge";
+      } else {
+        $talks[$index]['venue'] = $talk->venue;        
+      }
       $index++;
     }
 
