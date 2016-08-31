dc85008040ec3e3369b8382daa30bf5e85b5b790
Javier Arias <ja573@newton.ac.uk>
2016-08-31 15:16:30 +0100

    changed  address to info@ukfluids in contact form

--- app/Page.php
+++ app/Page.php
@@ -9,11 +9,16 @@ class Page extends Model
   /**
    * Functions sends email from Contact Us form
    * 
+   * @access public
+   * @static
    * @author Robert Barczyk <robert@barczyk.net>
+   * @param string $name
+   * @param string $from
+   * @param string $message
    */
   public static function sendForm($name, $from, $message) 
   {
-    $to       = 'webmaster@ukfluids.net';
+    $to       = 'info@ukfluids.net';
     $subject  = 'Message from UKFN website';
     $headers  = 'From: ' . $name . ' <' .$from .'>' . "\r\n";
     $headers .= 'Reply-To: ' . $from . '' . "\r\n";
