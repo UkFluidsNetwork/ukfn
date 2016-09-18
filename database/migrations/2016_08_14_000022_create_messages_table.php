3887bd403ed8e610adcc166bc911be67a5adf8bd
Javier Arias <ja573@cam.ac.uk>
2016-09-18 18:50:37 +0100

    from and to attributes in messages

--- database/migrations/2016_08_14_000022_create_messages_table.php
+++ database/migrations/2016_08_14_000022_create_messages_table.php
@@ -14,6 +14,8 @@ class CreateMessagesTable extends Migration
     {
         Schema::create('messages', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('from', 255);
+            $table->string('to', 255);
             $table->string('subject', 255);
             $table->longtext('body');
             $table->integer('user_id')->unsigned();
