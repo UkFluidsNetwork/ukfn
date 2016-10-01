7499653455c9dc40dd705cb3f64af87a38f8b71d
Javier Arias <ja573@cam.ac.uk>
2016-10-01 14:04:57 +0100

    Added messages attachment migration

---
+++ database/migrations/2016_10_01_133852_alter_messages_table.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class AlterMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('messages', function ($table) {
+            $table->string('attachment', 255)->after('body');
+        });
+    }
+
+}
