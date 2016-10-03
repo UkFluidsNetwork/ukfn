af49145d556b4f4a32eb75d7a9188b5ed044b8c7
Robert Barczyk <robert@barczyk.net>
2016-10-03 23:48:39 +0100

    Added imperial talks

--- app/Http/Controllers/TalksController.php
+++ app/Http/Controllers/TalksController.php
@@ -19,12 +19,14 @@ class TalksController extends Controller {
 
         $exceptions = ["TBC", "tbc", "To be confirmed", "Title to be confirmed", "TBD"];
 
-        
-        $xmlLink = "http://talks.cam.ac.uk/show/xml/54169";
+        $xmlCam = "http://talks.cam.ac.uk/show/xml/54169";
+        $xmlImperial = 'http://www3.imperial.ac.uk/imperialnewsevents/eventsfront?pid=69_189112051_69_189111978_189111978';
 
-        $talkAggregator = simplexml_load_file($xmlLink);
+        $unicam = simplexml_load_file($xmlCam);       
+        $imperial = simplexml_load_file($xmlImperial);
 
-        foreach ($talkAggregator->talk as $value) {
+        // update uni of cam
+        foreach ($unicam->talk as $value) {
             $existingTalk = DB::table('talks')->where('talkid', '=', $value->id)->get();
             
             // Insert new record if fetched talk doesn't exist in our DB 
@@ -41,7 +43,7 @@ class TalksController extends Controller {
                 $talk->venue = $value->venue;
                 $talk->organiser = $value->organiser;
                 $talk->series = $value->series;
-                $talk->aggregator = $talkAggregator->id;
+                $talk->aggregator = $unicam->id;
                 $talk->abstract = $value->abstract;
                 $talk->message = $value->special_message;
                 $talk->created_at = Carbon::createFromFormat('D, d M Y H:i:s e', $value->created_at)->format('Y-m-d H:i:s');
@@ -62,7 +64,7 @@ class TalksController extends Controller {
                 $talk->venue = $value->venue;
                 $talk->organiser = $value->organiser;
                 $talk->series = $value->series;
-                $talk->aggregator = $talkAggregator->id;
+                $talk->aggregator = $unicam->id;
                 $talk->abstract = $value->abstract;
                 $talk->message = $value->special_message;
                 $talk->created_at = Carbon::createFromFormat('D, d M Y H:i:s e', $value->created_at)->format('Y-m-d H:i:s');
@@ -72,6 +74,51 @@ class TalksController extends Controller {
             }
         }
         
+        // imperial talks
+        foreach ($imperial as $value) {
+            // there are some empty values at the begining of the string
+            if ($value->id) {
+                $namespaces = $value->getNamespaces(true);
+                $imperialnewsevents = $value->children($namespaces["imperialnewsevents"]);
+                
+                $existingTalk = DB::table('talks')->where('talkid', '=', $imperialnewsevents->articleid)->get();
+                
+                // Insert new record if fetched talk doesn't exist in our DB 
+                if (empty($existingTalk)) {
+                    // create talk object and assign attributes
+                    $talk = new Talk();
+                    $talk->talkid = $imperialnewsevents->articleid;
+                    $talk->title = $value->title;
+                    $talk->speaker = $value->summary;
+                    $talk->start = Carbon::parse($imperialnewsevents->event_start_date);
+                    $talk->end = Carbon::parse($imperialnewsevents->event_end_date);
+                    $talk->url = $value->id;
+                    $talk->venue = $imperialnewsevents->source . ", " . $imperialnewsevents->location;
+                    $talk->organiser = $imperialnewsevents->source;
+                    $talk->created_at = Carbon::parse($value->published);
+                    $talk->updated_at = Carbon::parse($value->updated);
+                    $talk->deleted = 0;
+                    $talk->save();
+                }
+                // Otherwise update existing one
+                else {
+                    $talk = Talk::find($existingTalk[0]->id);
+                    $talk->talkid = $imperialnewsevents->articleid;
+                    $talk->title = $value->title;
+                    $talk->speaker = $value->summary;
+                    $talk->start = Carbon::parse($imperialnewsevents->event_start_date);
+                    $talk->end = Carbon::parse($imperialnewsevents->event_end_date);
+                    $talk->url = $value->id;
+                    $talk->venue = $imperialnewsevents->source . ", " . $imperialnewsevents->location;
+                    $talk->organiser = $imperialnewsevents->source;
+                    $talk->created_at = Carbon::parse($value->published);
+                    $talk->updated_at = Carbon::parse($value->updated);
+                    $talk->deleted = 0;
+                    $talk->save();
+                }   
+            }
+        }
+                
         $talks = [];
         $index = 0;
         $allTalks = Talk::getAllTalks();
@@ -80,6 +127,7 @@ class TalksController extends Controller {
             if (in_array($talk->title, $exceptions)) {
                 continue;
             }
+            
             $talks[$index]['title'] = $talk->title;
             $talks[$index]['when'] = date("l jS F", strtotime($talk->start)) . " at " . date("H:i", strtotime($talk->start));
             $talks[$index]['speaker'] = $talk->speaker;
