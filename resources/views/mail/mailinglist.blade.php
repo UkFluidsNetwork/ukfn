924a90022336f091e71023a346767fd34af95b70
Javier Arias <ja573@cam.ac.uk>
2016-10-03 17:29:14 +0100

    reword of mailing list blurb

--- resources/views/mail/mailinglist.blade.php
+++ resources/views/mail/mailinglist.blade.php
@@ -2,5 +2,5 @@
 <br><br><br><br>
 <p class="text-muted" style='font-size:12px;'>
   {{ Html::link($unsubscribe, 'Unsubscribe') }} | Contact Us: {{ Html::link('mailto:info@ukfluids.net', 'info@ukfluids.net') }} <br><br>
-  You are receiving this email because {{ Html::link('mailto:' . $address, $address) }} is registered in UKFN's mailing list.
+  You are receiving this email because {{ Html::link('mailto:' . $address, $address) }} is subscribed to the UKFN mailing list.
 </p>
