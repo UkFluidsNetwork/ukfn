ae92e3c1fca31003f7a8af35590b3682154bdfd7
Javier Arias <ja573@cam.ac.uk>
2016-11-23 15:11:07 +0000

    institutions crud

---
+++ app/Http/Controllers/InstitutionsController.php
@@ -0,0 +1,158 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\InstitutionsFormRequest;
+use Illuminate\Support\Facades\Session;
+use Auth;
+use App\Institution;
+use App\Institutiontype;
+
+class InstitutionsController extends Controller
+{
+
+    /**
+     * List all institutions
+     * @author Javier Arias <ja573@cam.ac.uk>
+     * @access public
+     * @return void
+     */
+    public function view()
+    {
+        if (!PanelController::checkIsAdmin()) {
+            return redirect('/');
+        }
+
+        $bread = [
+            ['label' => 'Home', 'path' => '/'],
+            ['label' => 'Panel', 'path' => '/panel'],
+            ['label' => 'Institutions', 'path' => '/panel/institutions']
+        ];
+        $breadCount = count($bread);
+
+        $institutions = Institution::all();
+        foreach ($institutions as $institution) {
+            $institution->created = date("d M H:i", strtotime($institution->created_at));
+            $institution->updated = date("d M H:i", strtotime($institution->updated_at));
+        }
+
+        return view('panel.institutions.view', compact('institutions', 'bread', 'breadCount'));
+    }
+
+    /**
+     * Edit an institution
+     * @author Javier Arias <ja573@cam.ac.uk>
+     * @access public
+     * @param int $id
+     * @return void
+     */
+    public function edit($id)
+    {
+        $admin = new PanelController();
+        if (!$admin->checkIsAdmin()) {
+            return redirect('/');
+        }
+
+        $bread = [
+            ['label' => 'Home', 'path' => '/'],
+            ['label' => 'Panel', 'path' => '/panel'],
+            ['label' => 'Institutions', 'path' => '/panel/institutions'],
+            ['label' => 'Edit', 'path' => '/panel/institutions/edit'],
+        ];
+        $breadCount = count($bread);
+
+        $institution = Institution::findOrFail($id);
+        $institutiontypes = Institutiontype::lists('name', 'id');
+
+        return view('panel.institutions.edit', compact('institution', 'bread', 'breadCount', 'institutiontypes'));
+    }
+
+    /**
+     * Update an institution
+     * @author Javier Arias <ja573@cam.ac.uk>
+     * @access public
+     * @param int $id
+     * @param InstitutionsFormRequest $request
+     * @return void
+     */
+    public function update($id, InstitutionsFormRequest $request)
+    {
+        try {
+            $institution = Institution::findOrFail($id);
+            $input = $request->all();
+            $institution->fill($input);
+            $institution->save();
+            Session::flash('success_message', 'Edited succesfully.');
+        } catch (Exception $ex) {
+            Session:flash('error_message', $ex);
+        }
+        return redirect('/panel/institutions');
+    }
+
+    /**
+     * Add an institution
+     * @author Javier Arias <ja573@cam.ac.uk>
+     * @access public
+     * @param int $id
+     * @return void
+     */
+    public function add()
+    {
+        $admin = new PanelController();
+        if (!$admin->checkIsAdmin()) {
+            return redirect('/');
+        }
+
+        $bread = [
+            ['label' => 'Home', 'path' => '/'],
+            ['label' => 'Panel', 'path' => '/panel'],
+            ['label' => 'Institutions', 'path' => '/panel/institutions'],
+            ['label' => 'Add', 'path' => '/panel/institutions/add'],
+        ];
+        $breadCount = count($bread);
+        $institutiontypes = Institutiontype::lists('name', 'id');
+
+        return view('panel.institutions.add', compact('bread', 'breadCount', 'institutiontypes'));
+    }
+
+    /**
+     * Create an institution
+     * @author Javier Arias <ja573@cam.ac.uk>
+     * @access public
+     * @param InstitutionsFormRequest $request
+     * @return void
+     */
+    public function create(InstitutionsFormRequest $request)
+    {
+        try {
+            $institution = new Institution;
+            $input = $request->all();
+            $institution->fill($input);
+            $institution->save();
+            Session::flash('success_message', 'Added succesfully.');
+        } catch (Exception $ex) {
+            Session:flash('error_message', $ex);
+        }
+        return redirect('/panel/institutions');
+    }
+
+    /**
+     * Delete an institution
+     * @author Javier Arias <ja573@cam.ac.uk>
+     * @access public
+     * @param int $id
+     * @return void
+     */
+    public function delete($id)
+    {
+        try {
+            $institution = Institution::findOrFail($id);
+            $institution->deleted = 1;
+            $institution->save();
+            Session::flash('success_message', 'Deleted successfully.');
+        } catch (Exception $ex) {
+            Session:flash('error_message', $ex);
+        }
+        return redirect('/panel/institutions');
+    }
+}
---
+++ resources/views/panel/institutions/edit.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.admin')
+@section('admincontent')
+
+<h2 class='line-break'>Edit: {{ $institution->name }}</h2>
+
+{!! Form::model($institution, [
+'method' => 'PATCH',
+'action' => ['InstitutionsController@update', $institution->id],
+'class' => 'form-horizontal'
+]) !!}
+<div class='form-group {{ $errors->has('name') ? ' has-error line-break-dbl' : '' }}'>
+  {!! Form::label('name', 'Institution:', ['class' => 'control-label col-lg-2 text-left']) !!}
+  <div class=' col-lg-8'>
+    {!! Form::text('name', $institution->name, ['class' => 'form-control','placeholder' => 'The name of the institution']) !!}
+    @if ($errors->has('name'))
+    <span class="text-danger">
+      <span>{{ $errors->first('name') }}</span>
+    </span>
+    @endif
+  </div>
+</div>
+<div class='form-group {{ $errors->has('type') ? ' has-error line-break-dbl' : '' }}'>
+  {!! Form::label('institutiontype_id', 'Type:', ['class' => 'control-label col-lg-2 text-left']) !!}
+  <div class=' col-lg-8'>
+    {!! Form::select('institutiontype_id', $institutiontypes, $institution->institutiontype_id, ['class' => 'selectpicker show-tick']) !!}
+    @if ($errors->has('type'))
+    <span class="text-danger">
+      <span>{{ $errors->first('type') }}</span>
+    </span>
+    @endif
+  </div>
+</div>
+<div class=' col-lg-offset-2 col-lg-8'>
+  <div class='form-group line-break-dbl-top'>
+    {!! Form::submit('Save', ['class' => 'btn btn-success btn-lg2']) !!}
+  </div>    
+</div>
+{!! Form::close() !!}
+@endsection
