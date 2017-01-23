ef4b60f30678a79964152e464cb15ddce1da78a2
Robert Barczyk <robert@barczyk.net>
2017-01-23 23:15:20 +0000

    DTP added

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -13,6 +13,7 @@
         <link href="{{ asset('css/bootstrap-select.min.css') }}" rel="stylesheet" type="text/css">
         <link href="{{ asset('css/vendor/selectize.css') }}" rel="stylesheet" type="text/css">
         <link href="{{ asset('css/vendor/selectize.bootstrap3.css') }}" rel="stylesheet" type="text/css">
+        <link href="{{ asset('css/vendor/bootstrap-datetimepicker/bootstrap-datetimepicker.min.css') }}" rel="stylesheet" type="text/css">
         <script src="{{ asset('js/jquery-2.2.4.min.js')}}"></script>
         <script src="{{ asset('js/vendor/angular.js')}}"></script>
         <script src="{{ asset('js/vendor/angular-messages.js')}}"></script>
@@ -25,6 +26,9 @@
         <script src="{{ asset('js/angCtrl.js')}}"></script>
         <script src="{{ asset('js/bootstrap-select.min.js')}}"></script>
         <script src="{{ asset('js/vendor/selectize.min.js')}}"></script>
+        <script src="{{ asset('js/vendor/moment/moment.js')}}"></script>
+        <script src="{{ asset('/js/vendor/bootstrap-datetimepicker/bootstrap-datetimepicker.min.js')}}"></script>
+
         <!-- favicon -->
         <meta name="msapplication-TileColor" content="#ffffff">
         <meta name="msapplication-TileImage" content="{{ asset('pictures/favicon/ms-icon-144x144.png?v=2') }}">
