8bf2efb8ccde8dee1fc7666fba9b61acad064f13
Robert Barczyk <robert@barczyk.net>
2017-02-08 23:58:29 +0000

    Moved menu to right

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -13,10 +13,10 @@
     </ol>
     <div class="container-fluid nopadding">
         <div class="row">
-            <div class="col-lg-2 col-lg-push-10 col-md-2 col-md-push-10 col-sm-2 col-sm-push-10 col-xs-12 col-xs-push-0">
+            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
                 @include('panel.menu')
             </div>
-            <div class="col-lg-10 col-lg-pull-2 col-md-10 col-md-pull-2 col-sm-10 col-sm-pull-2 col-xs-12 col-xs-pull-0">
+            <div class="col-lg-10 col-md-10 col-sm-10 col-xs-12">
                 @yield('admincontent')
             </div>
         </div>
--- resources/views/panel/menu.blade.php
+++ resources/views/panel/menu.blade.php
@@ -77,7 +77,7 @@
                {{ Request::is('panel/sendmail*', 'panel/subscriptions*', 'panel/messages*') ? 'active' : '' }}">Mailing</a>
             <ul id='admin-subnav-mailing' class='nav nav-stacked' >
                 <li><a href="{{ URL::to('panel/sendmail') }}" class="{{ Request::is('panel/sendmail') ? 'active' : '' }}">Send Mail</a></li>
-                <li><a href="{{ URL::to('panel/subscriptions') }}" class="{{ Request::is('panel/subscriptions/') ? 'active' : '' }}">Subscriptions</a></li>
+                <li><a href="{{ URL::to('panel/subscriptions') }}" class="{{ Request::is('panel/subscriptions') ? 'active' : '' }}">Subscriptions</a></li>
             </ul>
         </li>
     </ul> 
