3a8de917f271a4f1a444832e03a28260c0b6c918
Javier Arias <ja573@cam.ac.uk>
2017-02-16 12:57:30 +0000

    update srv proposal document

--- resources/views/srv/index.blade.php
+++ resources/views/srv/index.blade.php
@@ -9,7 +9,7 @@
       The following pdf gives the context of the call and sets out the information you need to provide in your proposal:
     <p>
     <p>
-      <a href="{{ asset('files/UKFN_SRVs_call_Rev.161212.pdf') }}">[UKFN_SRVs_call_Rev.161212.pdf]</a>
+      [<a href="{{ asset('files/UKFN_SRVs_call_170216.pdf') }}">UKFN_SRVs_call_170216.pdf</a>]
     </p>
     <p>
       Proposals will be assessed in batches every 4 months. The next deadline is 31 May 2017.
