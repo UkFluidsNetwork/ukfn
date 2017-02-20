fbfc80b624f954a8f24a757f2efc7c8c4f777b02
Javier Arias <ja573@cam.ac.uk>
2017-02-20 20:38:57 +0000

    added srv visits in static form

--- resources/views/srv/index.blade.php
+++ resources/views/srv/index.blade.php
@@ -15,5 +15,163 @@
       Proposals will be assessed in batches every 4 months. The next deadline is 31 May 2017.
     </p>
   </div>
+  
+    <section class="page-header line-break-dbl">
+        <h3 class="text-danger line-break">Advancing Atmospheric Models: Aspects of Spatial Discretisation</h3>
+        
+        <span class="talks-speaker display-block">Dr Joanna Szmelter</span>
+        <span class="display-block">Wolfson School of Mechanical, Electrical and Manufacturing Engineering</span>
+        <span class="display-block line-break">Loughborough University</span>
+        <span class="display-block line-break">Visiting: Dr Nils Wedi, Dr Christan Kühnlein & Prof Piotr Smolarkiewicz, ECMWF (European Centre for Medium-Range Weather Forecasts)</span>
+        <div class="line-break-dbl-top display-block line-breakdbl">
+            <p>
+                ECMWF is currently developing a ground breaking non-hydrostatic atmospheric dynamics Finite Volume Module (FVM)
+                which will be implemented in the ECMWF’s Integrated Forecasting System (IFS). 
+                The IFS is the operational tool used to provide medium range weather prediction services for the UK and most EU countries. 
+            </p>
+            <p>
+                Many aspects of the spatial discretisation employed in the FVM originated from Dr Szmelter’s earlier 
+                work on unstructured mesh models for fluid flows. In recent years, these have been substantially advanced 
+                by ECMWF to take advantage of the IFS environment and High Performance Computing.
+            </p>
+            <p>
+                The main goal of the SRV is to discuss and initiate the implementation of an alternative discretisation 
+                of selected operators, which could potentially offer further improvements to the module in terms of accuracy and speed.
+                Suitable numerical tests will be defined and set up. Dr Szmelter will also explore whether some of the 
+                advancements achieved for atmospheric flows in the FVM could be applied to other engineering and environmental flows.
+            </p>
+        </div>
+    </section>
+  
+    <section class="page-header line-break-dbl">
+        <h3 class="text-danger line-break">Controlling nematic microfluidics: a merger of modelling, simulation and experiments</h3>
+        
+        <span class="talks-speaker display-block">Dr Apala Majumdar</span>
+        <span class="display-block">Department of Mathematical Sciences</span>
+        <span class="display-block line-break">University of Bath</span>
+        <span class="display-block line-break">Visiting: Dr Ian Griffiths (Mathematical Institute, University of Oxford)</span>
+        <div class="line-break-dbl-top display-block line-breakdbl">
+            <p>
+                Nematic liquid crystals (NLCs) are complex non-Newtonian fluids – anisotropic fluids with a degree of 
+                long-range orientational ordering. A rapidly growing research theme concerns “nematic microfluidics” or 
+                the flow of NLCs confined to thin channels. Experimentalists are keen to exploit nematic microfluidics 
+                for new applications in hydrodynamics, transport phenomena and next-generation pharmaceutical applications such as drug delivery.
+            </p>
+            <p>
+                The introduction of nanoparticles into NLCs modifies the fluid properties, and understanding this effect 
+                may lead to new applications. Some earlier collaborative work between Drs Majumdar and Griffiths on the 
+                flow of nanoparticles in nematic microfluidics showed interesting effects, especially in cases with multiple nanoparticles. 
+            </p>
+            <p>
+                The SRV will therefore focus primarily on the flow of nanoparticles in nematic microfluidics, 
+                and through a combination of modelling, simulation and experiments, will address issues such as: 
+            <ul>
+                <li>how the nanoparticles interact with the fluid flow and the nematic orientation, and vice-versa;</li>
+                <li>how this interaction can be manipulated to produce desired agglomerates with desired properties;</li>
+                <li>how the shape and size of nanoparticles can be varied to manipulate the rheology;</li>
+                <li>assessing the predictions of different mathematical theories for nematodynamics and how they compare to experiments.</li>
+            </ul>
+            </p>
+        </div>
+    </section>
+  
+    <section class="page-header line-break-dbl">
+        <h3 class="text-danger line-break">Exploration of integrated microfluidics with phononics and acoustofluidics based on thin-film platform</h3>
+        
+        <span class="talks-speaker display-block">Dr Richard Fu</span>
+        <span class="display-block">Department of Maths, Physics and Electrical Engineering</span>
+        <span class="display-block line-break">Northumbria University</span>
+        <span class="display-block line-break">Visiting: Dr Julien Reboud & Dr Rab Wilson (University of Glasgow)</span>
+        <div class="line-break-dbl-top display-block line-breakdbl">
+            <p>
+                The SRV will initiate proof-of-concept collaborative work to develop phononic structures (Glasgow) on 
+                thin-film ZnO surface acoustic wave devices (Northumbria). This has the potential of creating ultra-low-cost 
+                acoustofluidics devices, capable of carrying out complex microfluidic processing of samples for applications 
+                in flexible and wearable healthcare monitoring. Dr Fu will fabricate and test different designs of acoustofluidic device.
+            </p>
+            <p>
+                Prior to the SRV, suitable ZnO-coated substrate samples will be prepared at Northumbria, 
+                while Glasgow will carry out calculations to confirm the microstructure geometry to be used. Then:
+                <ul>
+                    <li>
+                        In the James Watt Nanofabrication Centre at Glasgow, different phononic structures (pillars or holes) 
+                        will be patterned and fabricated on the ZnO-film-coated substrates
+                    </li>
+                    <li>The ultrasonic surface vibration patterns will be validated using a high-frequency laser Doppler vibrometer (frequency range in the 10’s of MHz)</li>
+                    <li>
+                        The microfluidic performance of fabricated devices will be tested using (i) conventional microfluidic test beds to look at streaming, 
+                        flowing, jetting and nebulisation; and (ii) a high-frame-rate camera (up to 1MHz)
+                    </li>
+                </ul>
+            </p>
+            <p>
+                Following the SRV, the data will be used in support of new collaborative research bids, and in discussions 
+                with interested industrial partners who could develop microfluidics products for healthcare applications.
+            </p>
+        </div>
+    </section>
+  
+    <section class="page-header line-break-dbl">
+        <h3 class="text-danger line-break">Interrogating local shear effects on coherent structure identification in turbulent flows</h3>
+        
+        <span class="talks-speaker display-block">Dr Chris Keylock</span>
+        <span class="display-block"Department of Civil and Structural Engineering</span>
+        <span class="display-block line-break">University of Sheffield</span>
+        <span class="display-block line-break">Visiting: Dr Oliver Buxton (Department of Aeronautics, Imperial College, London)</span>
+        <div class="line-break-dbl-top display-block line-breakdbl">
+            <p>
+                Dr Buxton has obtained three-dimensional experimental data on the behaviour of various turbulent flows 
+                (boundary layers, wakes, shear layers). With 3D data, the full velocity gradient tensor is available for 
+                analysis, and we will apply various vortex identification schemes to extract flow structure information. 
+                However, most of these methods are eigenvalue-based, so we will also derive complementary methods that 
+                incorporate effects excluded from consideration in an eigenvalue-based approach. Hence, we will be able 
+                to identify structures that are clear in both types of approach, as well as those that are better 
+                represented in one than the other. We will then seek to explain these differences in terms of local 
+                energy production and dissipation. Such analyses will inform the physical basis for future turbulence closures.
+            </p>
+        </div>
+    </section>
+  
+    <section class="page-header line-break-dbl">
+        <h3 class="text-danger line-break">Towards a mechanistic understanding of haematocrit changes in tumour vasculature</h3>
+        
+        <span class="talks-speaker display-block">Dr Miguel O. Bernabeu</span>
+        <span class="display-block">Centre for Medical Informatics</span>
+        <span class="display-block line-break">The University of Edinburgh</span>
+        <span class="display-block line-break">Visiting: Prof Helen Byrne (Mathematical Institute, University of Oxford)</span>
+        <div class="line-break-dbl-top display-block line-breakdbl">
+            <p>
+                Several authors have reported anomalous blood flow patterns in tumour vasculature, including deviations 
+                from the typical haematocrit (red blood cell count) distributions observed in healthy tissue. 
+                Such abnormalities present a challenge for drug delivery and have been linked to tumour hypoxia and angiogenesis.
+            </p>
+            <p>
+                To date, most computational models of tumour blood flow view the blood as a homogeneous fluid and employ 
+                phenomenological rules to determine haematocrit changes at vessel bifurcations. Such models fail to capture 
+                the dynamics encountered in tumours. This is, in part, due to the computational challenges associated with 
+                simulating haematocrit changes in a mechanistic way, i.e. using a model of interacting deformable particles 
+                to describe the transport of red blood cells (RBCs) in the plasma.
+            </p>
+            <p>
+                The SRV will initiate a collaboration between the groups of Prof Byrne and Dr Bernabeu to exploit their complementary expertise:
+                <ul>
+                    <li>
+                        Prof Byrne and colleagues have considerable experience of simulating blood flow and oxygen 
+                        distribution in tumours and have recently developed a microfluidics assay that recapitulates RBC dynamics in tumour vascular networks.
+                    </li>
+                    <li>
+                        Dr Bernabeu and colleagues have recently extended HemeLB, their blood flow simulation platform 
+                        that treats blood as a suspension of red blood cells 
+                        (<a href="http://www.archer.ac.uk/community/eCSE/eCSE01-010/eCSE01-010.php">http://www.archer.ac.uk/community/eCSE/eCSE01-010/eCSE01-010.php)</a>.
+                    </li>
+                </ul>
+            </p>
+            <p>
+                The SRV will focus on constructing and validating computational models of blood flow in realistic tumour 
+                microvasculature, based on experimental data recently obtained by Prof Byrne and colleagues. These models 
+                will be used to develop a mechanistic model of haematocrit changes.
+            </p>
+        </div>
+    </section>
 
 @endsection
