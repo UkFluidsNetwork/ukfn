fe638a121d4342fde124979c79f720ef0c05d1fc
Javier Arias <ja573@cam.ac.uk>
2017-03-08 10:23:09 +0000

    add phpdocs for new functions in user

--- app/User.php
+++ app/User.php
@@ -110,7 +110,7 @@ class User extends Authenticatable
     {
         return $this->belongsToMany('App\Sig', 'sig_users')->withPivot('main')->withTimestamps();
     }
-    
+
     /**
      * Get the tags associated with the given user
      * 
@@ -202,68 +202,133 @@ class User extends Authenticatable
     {
         return $this->institutions->lists('id')->toArray();
     }
-    
+
+    /**
+     * Determine if the user is allowed to edit the sig given by $sigId
+     * 
+     * @param int $sigId
+     * @return boolean
+     */
     public function canEditSig($sigId)
     {
         return $this->isAdmin() || $this->isLeaderOfSig($sigId);
     }
-    
+
+    /**
+     * Determine if the user is a leader of the sig given by $sigId
+     * 
+     * @param int $sigId
+     * @return boolean
+     */
     public function isLeaderOfSig($sigId)
     {
         return $this->sigs()->where('sigs.id', $sigId)->where('main', 1)->count() > 0;
     }
-    
+
+    /**
+     * Determine if the user is asscociated with the sig given by $sigId
+     * 
+     * @param int $sigId
+     * @return boolean
+     */
     public function belongsToSig($sigId)
     {
         return $this->sigs()->where('sigs.id', $sigId)->count() > 0;
     }
-    
+
+    /**
+     * Determine if the user is an administrator.
+     * 
+     * @return boolean
+     */
     public function isAdmin()
     {
         return $this->group_id === 1;
     }
-    
+
+    /**
+     * Determine if the user is the leader of at least one sig.
+     * 
+     * @return boolean
+     */
     public function isSigLeader()
     {
         return $this->sigs()->where('main', 1)->count() > 0;
     }
-    
+
+    /**
+     * Determine if the user is the co-leader of at least one sig.
+     * 
+     * @return boolean
+     */
     public function isSigCoLeader()
     {
         return $this->sigs()->where('main', 2)->count() > 0;
     }
-    
+
+    /**
+     * Determine if the user is the key personnel of at least one sig.
+     * 
+     * @return boolean
+     */
     public function isSigKeyPersonnel()
     {
         return $this->sigs()->where('main', 3)->count() > 0;
     }
-    
+
+    /**
+     * Determine if the user is the member of at least one sig.
+     * 
+     * @return boolean
+     */
     public function isSigMember()
     {
         return $this->sigs()->where('main', 0)->count() > 0;
     }
-    
+
+    /**
+     * Get the SIG for which the user is a leader
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
     public function sigLeaderships()
     {
         return $this->sigs()->where('main', 1)->get();
     }
-    
+
+    /**
+     * Get the IDs of the SIG for which the user is a leader
+     * 
+     * @return array
+     */
     public function sigLeadershipsIds()
     {
         return $this->sigs()->where('main', 1)->lists('id')->toArray();
     }
-    
-    public function sigStatusId($id)
+
+    /**
+     * Get the value of the sig_users.main attribute for the user and a given $sigId
+     * 
+     * @param int $sigId
+     * @return int
+     */
+    public function sigStatusId($sigId)
     {
-        if (!$this->belongsToSig($id)) {
+        if (!$this->belongsToSig($sigId)) {
             return null;
         }
-        return $this->sigs()->where('sigs.id', $id)->first()->pivot->main;
+        return $this->sigs()->where('sigs.id', $sigId)->first()->pivot->main;
     }
-    
-    public function sigStatus($id)
+
+    /**
+     * Get the membership status of the user for a given $sigId
+     * 
+     * @param int $sigId
+     * @return string
+     */
+    public function sigStatus($sigId)
     {
-        switch ($this->sigStatusId($id)) {
+        switch ($this->sigStatusId($sigId)) {
             case 0: return "Member";
             case 1: return "Leader";
             case 2: return "Co-leader";
