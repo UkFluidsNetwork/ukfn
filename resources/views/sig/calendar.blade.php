34dfd865567f18335aab31c5b6bc1f9a3ddaa95b
Javier Arias <javier@arias.re>
2017-04-02 14:50:35 +0100

    add sig calendar url to google calendar

--- resources/views/sig/calendar.blade.php
+++ resources/views/sig/calendar.blade.php
@@ -11,7 +11,7 @@
 
 <div id="google_calendar">
     
-<iframe src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showCalendars=0&amp;height=600&amp;wkst=2&amp;bgcolor=%23FFFFFF&amp;src=4utcij1bn8htlmec7094it7980%40group.calendar.google.com&amp;color=%2329527A&amp;ctz=Europe%2FLondon"
+<iframe src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showCalendars=0&amp;height=600&amp;wkst=2&amp;bgcolor=%23FFFFFF&amp;src=ce7h79qrsq4a9degfo9khe0tbk%40group.calendar.google.com&amp;color=%2329527A&amp;ctz=Europe%2FLondon"
         style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
 </div>
 
