e5f8c7b498ebe9fe72206dee38ec9958558b0c47
Javier Arias <javier@arias.re>
2017-05-07 16:24:51 +0100

    add id to sig name

--- resources/views/sig/index.blade.php
+++ resources/views/sig/index.blade.php
@@ -13,7 +13,8 @@
     <div class="ell">
         <p>
           <span class="glyphicon glyphicon-exclamation-sign"></span>
-          If you are interested in joining a SIG, please contact the SIG leader directly.
+          If you are interested in joining a SIG,
+          please contact the SIG leader directly.
           There may be a third call for SIG proposals in Spring 2018.
         </p>
     </div>
@@ -28,19 +29,35 @@
                 <ng-map center="@{{ sigCtrl.map.coordinates }}"
                         scrollwheel="false"
                         draggable="true"
-                        map-type-control="false" street-view-control="false"
-                        zoom-control-options="{style:'SMALL', position:'TOP_RIGHT'}" options='@{{ sigCtrl.options }}' zoom="6" class="mapHeight">
-                    <custom-marker ng-repeat="institution in sigCtrl.thisSig.data.institutions" position="@{{ institution.lat }},@{{ institution.lng }}"
-                        title="@{{institution.name}}" icon="@{{ sigCtrl.customIcon }}"><div class="map-pointer"></div>
+                        map-type-control="false"
+                        street-view-control="false"
+                        zoom-control-options="{style:'SMALL',
+                            position:'TOP_RIGHT'}"
+                        options='@{{ sigCtrl.options }}'
+                        zoom="6"
+                        class="mapHeight">
+                    <custom-marker
+                        ng-repeat="institution
+                          in sigCtrl.thisSig.data.institutions"
+                        position="@{{ institution.lat }},@{{ institution.lng }}"
+                        icon="@{{ sigCtrl.customIcon }}"
+                        title="@{{institution.name}}">
+                        <div class="map-pointer"></div>
                     </custom-marker>
-                    <custom-marker ng-if="sigCtrl.displayAll" ng-repeat="institution in sigCtrl.distinctInstitutions" position="@{{ institution.lat }},@{{ institution.lng }}"
-                                   title="@{{institution.name}}" icon="@{{ sigCtrl.customIcon }}"><div class="map-pointer"></div>
+                    <custom-marker
+                        ng-if="sigCtrl.displayAll"
+                        ng-repeat="institution in sigCtrl.distinctInstitutions"
+                        position="@{{ institution.lat }},@{{ institution.lng }}"
+                        icon="@{{ sigCtrl.customIcon }}"
+                        title="@{{institution.name}}">
+                        <div class="map-pointer"></div>
                     </custom-marker>
                 </ng-map>
             </div>
         </div>
         <!-- Sig institutions -->
-        <div class="col-lg-3 col-lg-pull-6 col-md-3 col-md-pull-6 mapHeight axis-y">
+        <div class="col-lg-3 col-lg-pull-6 col-md-3 col-md-pull-6
+                    mapHeight axis-y">
             <div class="line-break hidden-sm hidden-md hidden-lg"></div>
             <div ng-if="sigCtrl.displayAll">
                 <div class="page-header nomargin-top">
@@ -51,7 +68,8 @@
                 <p>
                     <strong class="line-break">Institutions</strong>
                 </p>
-                <ul ng-repeat="ins in sigCtrl.distinctInstitutions" class="no-li-style">
+                <ul ng-repeat="ins in sigCtrl.distinctInstitutions"
+                    class="no-li-style">
                     <li>
                         @{{ ins.name }}
                     </li>
@@ -59,12 +77,17 @@
             </div>
             <div ng-if="!sigCtrl.displayAll">
                 <div class="page-header" style="margin-top: 0px;">
-                    <a href="/sig/@{{sigCtrl.thisSig.data.shortname}}" class="text-danger">
+                    <a href="/sig/@{{sigCtrl.thisSig.data.shortname}}"
+                       class="text-danger">
                         <div class="text-danger line-break">
-                            <strong>@{{ sigCtrl.thisSig.data.name }}</strong>
+                            <strong>
+                                @{{ sigCtrl.thisSig.data.id }}.
+                                @{{ sigCtrl.thisSig.data.name }}
+                            </strong>
                         </div>
                     </a>
-                    <div ng-if="sigCtrl.thisSig.data.smallimage" class='sig-map-image' class="">
+                    <div ng-if="sigCtrl.thisSig.data.smallimage"
+                         class='sig-map-image'>
                         <a href="/sig/@{{sigCtrl.thisSig.data.shortname}}">
                             <img class='sig-map-image'
                                  src="/pictures/sig/@{{sigCtrl.thisSig.data.smallimage}}"
@@ -75,14 +98,17 @@
                         @{{ sigCtrl.thisSig.data.description }}
                     </p>
                     <p>
-                        <a href="/sig/@{{sigCtrl.thisSig.data.shortname}}">More details</a>
+                        <a href="/sig/@{{sigCtrl.thisSig.data.shortname}}">
+                          More details
+                        </a>
                     </p>
                 </div>
                 <div class="page-header" style="margin-top: 0px;">
                     <p>
                         <strong class="line-break">Leader</strong>
                     </p>
-                    <ul ng-repeat="leader in sigCtrl.thisSig.data.leader" class="no-li-style">
+                    <ul ng-repeat="leader in sigCtrl.thisSig.data.leader"
+                        class="no-li-style">
                         <li>
                             @{{ leader.name }} @{{ leader.surname }} <i>(<span ng-repeat="institution in leader.institutions">@{{ institution.name }}</span>)</i></li>
                     </ul>
@@ -90,7 +116,8 @@
                 <p>
                     <strong class="line-break">Institutions</strong>
                 </p>
-                <ul ng-repeat="ins in sigCtrl.thisSig.data.institutions" class="no-li-style">
+                <ul ng-repeat="ins in sigCtrl.thisSig.data.institutions"
+                    class="no-li-style">
                     <li>
                         @{{ ins.name }}</li>
                 </ul>
@@ -101,15 +128,19 @@
             <div class="line-break hidden-sm hidden-md hidden-lg"></div>
             <ul class="nav nav-pills nav-stacked">
                 <li ng-class="sigCtrl.displayAll ? 'active' : ''">
-                    <a href="" ng-click="sigCtrl.dispAll(); sigCtrl.setActive(false)">
+                    <a href=""
+                       ng-click="sigCtrl.dispAll(); sigCtrl.setActive(false)">
                         All SIGs
                     </a>
                 </li>
             </ul>
-            <ul class="nav nav-pills nav-stacked" ng-repeat="sig in sigCtrl.allSigs.data">
+            <ul class="nav nav-pills nav-stacked"
+                ng-repeat="sig in sigCtrl.allSigs.data">
                 <li ng-class="sigCtrl.sigActive === sig.id ? 'active' : ''" >
-                    <a href="" ng-click="sigCtrl.getSig(sig.id); sigCtrl.setActive(sig.id);">
-                        @{{ sig.name }}
+                    <a href=""
+                       ng-click="sigCtrl.getSig(sig.id);
+                                sigCtrl.setActive(sig.id);">
+                        @{{ sig.id }}. @{{ sig.name }}
                     </a>
                 </li>
             </ul>
