bd68965982ee880ca6c143ff971f42d324312ff6
Javier Arias <javier@arias.re>
2017-05-07 11:56:45 +0100

    change streaming protocl

--- resources/views/talks/stream.blade.php
+++ resources/views/talks/stream.blade.php
@@ -1,11 +1,14 @@
-<script type="text/javascript" src="//sms.cam.ac.uk/mediaplayer/jwplayer.js"></script>
-<script type="text/javascript">jwplayer.key = "NNXEsEAdRYJ6hNlTY5m43FsKNDixOpQA881GiROWssE=";</script> 
+<script type="text/javascript" src="//sms.cam.ac.uk/mediaplayer/jwplayer.js">
+</script>
+<script type="text/javascript">
+    jwplayer.key = "NNXEsEAdRYJ6hNlTY5m43FsKNDixOpQA881GiROWssE=";
+</script>
 
 <div id="streamer">Loading the player ...</div>
 
 <script type="text/javascript">
     jwplayer("streamer").setup({
-        "file": "rtmp://live1.sms.cam.ac.uk:1935/ENG/live",
+        "file": "rtsp://live1.sms.cam.ac.uk:1935/ENG/live",
         "autostart": true,
         "aspectratio": "16:9",
         "width": "100%",
