98768f2827e0ed6dfb511f3f9a2cc9e245c5def8
Javier Arias <javier@arias.re>
2017-05-22 20:12:19 +0100

    sig page

--- resources/views/sig/page.blade.php
+++ resources/views/sig/page.blade.php
@@ -1,8 +1,6 @@
 @extends('layouts.master')
 @section('content')
 
-<h2 class="line-break">{{ $sig->id }}. {{ $sig->name }}</h2>
-
 <div class="container-fluid nopadding-left">
     <div class="row">
         <div class="col-lg-12 col-md-12 col-sm-12">
@@ -22,22 +20,26 @@
                         @endif
                     </div>
                 </div>
-                <a title="Previous SIG" class="left carousel-control" href="{{$prevSigPath}}">
+                <a title="Previous SIG" class="left carousel-control"
+                   href="{{$prevSigPath}}">
                     <span class="glyphicon glyphicon-chevron-left"></span>
                     <span class="sr-only">Previous</span>
                 </a>
-                <a title="Next SIG" class="right carousel-control" href="{{$nextSigPath}}">
+                <a title="Next SIG" class="right carousel-control"
+                   href="{{$nextSigPath}}">
                     <span class="glyphicon glyphicon-chevron-right"></span>
                     <span class="sr-only">Next</span>
                 </a>
                 @if (false) <!-- FIXME -->
-                    <a title="SIG map" class="up carousel-control" href="{{$mapSigPath}}">
+                    <a title="SIG map" class="up carousel-control"
+                       href="{{$mapSigPath}}">
                         <span class="glyphicon glyphicon-chevron-up"></span>
                         <span class="sr-only">SIG map</span>
                     </a>
                 @endif
                 @if ($sig->url && false) <!-- FIXME -->
-                    <a title="Home page" class="down carousel-control" href="{{$sig->url}}">
+                    <a title="Home page" class="down carousel-control"
+                       href="{{$sig->url}}">
                         <span class="glyphicon glyphicon-chevron-down"></span>
                         <span class="sr-only">Home page</span>
                     </a>
@@ -46,6 +48,7 @@
         </div>
     </div>
 </div>
+<h2>{{ $sig->name }}</h2>
 <div class="container-fluid nopadding-left">
     <div class="row">
         <div class="col-lg-8 col-md-8 col-sm-8">
@@ -56,47 +59,56 @@
                         {{ $sig->description }}
                     </p>
                 </div>
-                @if (!empty($sig->leader) || !empty($sig->coleader))
                 <div class="bs-callout bs-callout-danger">
-                    <h4>Key personnel</h4>
-                    <p class="small"><i>L = leader; C = co-leader</i></p>
+                    <h4>Join SIG Mailing list</h4>
                     <p>
-                        @if (!empty($sig->leader))
-                        <b>[L]</b> {{ $sig->leader[0]->name }} {{ $sig->leader[0]->surname }} 
-                        (<i>{{ $sig->leader[0]->institutions[0]->name }}</i>):
-                        {{ Html::link('mailto:' . $sig->leader[0]->email, $sig->leader[0]->email) }}
-                        <br>
-                        @endif
-                        @if (!empty($sig->coleaders))
-                            @foreach ($sig->coleaders as $coleader)
-                            <b>[C]</b> {{ $coleader->name }} {{ $coleader->surname }} 
-                            (<i>{{ $coleader->institutions[0]->name }}</i>): 
-                            {{ Html::link('mailto:' . $coleader->email, $coleader->email) }}<br>
-                            @endforeach
-                        @endif
-                        @if (!empty($sig->personnel))
-                            @foreach ($sig->personnel as $p)
-                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{ $p->name }} {{ $p->surname }} 
-                            (<i>{{ $p->institutions[0]->name }}</i>): 
-                            {{ Html::link('mailto:' . $p->email, $p->email) }}<br>
-                            @endforeach
-                        @endif
+                      <div class="input-group">
+                        <label for="subscribe-sig-email"
+                               class="sr-only">Subscribe</label>
+                        <input type="text"
+                               id="subscribe-sig-email"
+                               class="form-control"
+                               placeholder="your@email.com">
+                        <span class="input-group-btn">
+                          <button class="btn btn-default"
+                                  type="button">Subscribe</button>
+                        </span>
+                      </div>
                     </p>
                 </div>
-                @endif
                 @if (!empty($sig->users))
                 <div class="bs-callout bs-callout-danger">
                     <h4>Members</h4>
+                    <p class="small">
+                      <i>L = leader; C = co-leader; * = Other key personnel</i>
+                    </p>
                     <p>
+                      <table>
                         @foreach ($sig->users as $member)
-                        @if ($member->url)
-                        {{ Html::link($member->url, $member->name . " " . $member->surname) }}
-                        @else
-                        {{ $member->name }} {{ $member->surname }} 
-                        @endif
-                        (<i>{{ $member->institutions[0]->name }}</i>):
-                        {{ Html::link('mailto:' . $member->email, $member->email) }}<br>
+                        <tr>
+                          <td>
+                          @if ($member->isLeaderOfSig($sig->id))
+                            <b>[L]</b>&nbsp;
+                          @elseif ($member->isColeaderOfSig($sig->id))
+                            <b>[C]</b>&nbsp;
+                          @elseif ($member->isKeyPersonnelOfSig($sig->id))
+                            <b>[*]</b>&nbsp;
+                          @endif
+                          </td>
+                          <td>
+                          @if ($member->url)
+                          {{ Html::link($member->url,
+                                    $member->name . " " . $member->surname) }}
+                          @else
+                          {{ $member->name }} {{ $member->surname }} 
+                          @endif
+                          (<i>{{ $member->institutions[0]->name }}</i>):
+                          {{ Html::link('mailto:' . $member->email,
+                                        $member->email) }}
+                          </td>
+                        </tr>
                         @endforeach
+                      </table>
                     </p>
                 </div>
                 @endif
@@ -104,7 +116,8 @@
                 <div class="bs-callout bs-callout-danger">
                     <h4>External website</h4>
                     <p>
-                        {{ Html::link($sig->url, $sig->url, ['class'=> '', 'target' => '_blank']) }}
+                        {{ Html::link($sig->url, $sig->url,
+                                      ['class'=> '', 'target' => '_blank']) }}
                     </p>
                 </div>
                 @endif
