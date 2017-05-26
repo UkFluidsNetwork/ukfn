f34dda50c784acc93bc9530a14e2a5976ece9270
Javier Arias <javier@arias.re>
2017-05-26 15:56:03 +0100

    rever calendr

--- resources/views/sig/calendar.blade.php
+++ resources/views/sig/calendar.blade.php
@@ -11,7 +11,7 @@
 
 <div id="google_calendar">
 
-<iframe src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showCalendars=0&amp;height=600&amp;wkst=2&amp;bgcolor=%23FFFFFF&amp;src=63lvbb4aa3h67q0jenehis3fo8%40group.calendar.google.com&amp;color=%2329527A&amp;ctz=Europe%2FLondon"
+<iframe src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showCalendars=0&amp;height=600&amp;wkst=2&amp;bgcolor=%23FFFFFF&amp;src=ce7h79qrsq4a9degfo9khe0tbk%40group.calendar.google.com&amp;color=%2329527A&amp;ctz=Europe%2FLondon"
         style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
 </div>
 
