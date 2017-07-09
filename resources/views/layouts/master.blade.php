5a37301bcbe6a5480272a7f6a07714518a601d26
Javier Arias <javier@arias.re>
2017-07-09 18:15:56 +0100

    add domain banner

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -83,7 +83,7 @@
         <!-- NAVBAR - START -->
         <div id="menu-bar" data-spy="affix" data-offset-top="40">
 
-        @if ($_SERVER['SERVER_NAME'] == "localhost")
+        @if ($_SERVER['SERVER_NAME'] == "ukfluids.net")
         <div style="width:100%;height:40px;background-color:#1B75BB;color:white;text-align:center;font-size:22px;padding:13px;padding-bottom:45px;">
           UK Fluids Network has a new domain: <a style="color:white;" href="fluids.ac.uk">fluids.ac.uk</a>
         </div>
