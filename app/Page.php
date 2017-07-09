45558228dd76a4824c6595ee7c58b9cacfd8f0bd
Javier Arias <javier@arias.re>
2017-07-09 19:49:28 +0100

    change contact

--- app/Page.php
+++ app/Page.php
@@ -8,17 +8,16 @@ class Page extends Model
 {
   /**
    * Functions sends email from Contact Us form
-   * 
+   *
    * @access public
    * @static
-   * @author Robert Barczyk <robert@barczyk.net>
    * @param string $name
    * @param string $from
    * @param string $message
    */
-  public static function sendForm($name, $from, $message) 
+  public static function sendForm($name, $from, $message)
   {
-    $to       = 'info@ukfluids.net';
+    $to       = 'info@fluids.ac.uk';
     $subject  = 'Message from UKFN website';
     $headers  = 'From: ' . $name . ' <' .$from .'>' . "\r\n";
     $headers .= 'Reply-To: ' . $from . '' . "\r\n";
