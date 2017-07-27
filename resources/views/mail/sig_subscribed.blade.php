978394d80850fb143354ee86eb57c84dd8650820
Javier Arias <javier@arias.re>
2017-07-27 22:58:19 +0100

    add email template for sig subscriptions

---
+++ resources/views/mail/sig_subscribed.blade.php
@@ -0,0 +1,6 @@
+Thank you for signing up to our mailing list.
+<br><br>
+Your email address {{ $email }} has been added to our records, and the SIG
+leader has been notified of your subscription.
+<br><br>
+UK Fluids Network
