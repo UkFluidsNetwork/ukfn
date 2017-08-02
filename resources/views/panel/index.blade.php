49af3a02a021d8b619abf57d63eaf5f34f5b2d4c
Javier Arias <javier@arias.re>
2017-08-02 09:33:51 +0100

    remove email client details from one

--- resources/views/panel/index.blade.php
+++ resources/views/panel/index.blade.php
@@ -6,14 +6,6 @@
   <p>
     Please use the menu on the left to navigate through the admin sections.
   </p>
-  <p>
-    There is currently limited functionality, but this section will grow over the next months in order to provide
-    full control over all modules of the application.
-  </p>
 </div>
-<p>
-  Email client details (<a href="https://mail.one.com">https://mail.one.com</a>): <br>
-  <b>Email:</b> info@ukfluids.net<br>
-  <b>Password:</b> 8$Yy_>rPb8
-</p>
-  @endsection
+
+@endsection
