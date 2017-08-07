e7397b0cf88c9683603ddffc19fdd1ca94f351c5
Javier Arias <javier@arias.re>
2017-08-07 12:21:40 +0100

    tidy up requests

--- app/Http/Requests/SigSuggestionFormRequest.php
+++ app/Http/Requests/SigSuggestionFormRequest.php
@@ -4,34 +4,32 @@ namespace App\Http\Requests;
 
 use App\Http\Requests\Request;
 
-
 class SigSuggestionFormRequest extends Request
 {
+
     /**
      * Determine if the user is authorized to make this request.
      *
      * @return bool
-     * @author Robert Barczyk <robert@barczyk.net>
      */
     public function authorize()
     {
-      // allow everybody  
-      return TRUE;
+        return true;
     }
 
     /**
      * Get the validation rules that apply to the request.
      *
      * @return array
-     * @author Robert Barczyk <robert@barczyk.net>
      */
     public function rules()
     {
-      return [
-        'name' => 'required', 
-        'suggestion' => 'required',
-        'email' => 'required|email',
-        'institution' => 'required'
-      ];
+        return [
+          'name' => 'required',
+          'suggestion' => 'required',
+          'email' => 'required|email',
+          'institution' => 'required'
+        ];
     }
 }
+
