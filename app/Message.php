e441a84a347c1bc2a4fb606846b98b7328d7e49a
Javier Arias <javier@arias.re>
2017-08-07 12:23:09 +0100

    tidy up message

--- app/Message.php
+++ app/Message.php
@@ -28,10 +28,8 @@ class Message extends Model
 
     /**
      * Get all messages
+     *
      * @return array
-     * @static
-     * @access public
-     * @author Robert Barczyk <robert@barczyk.net>
      */
     public static function getAll()
     {
@@ -40,25 +38,25 @@ class Message extends Model
 
     /**
      * Get all messages sent to the mailing list
-     * @author Javier Arias <ja573@cam.ac.uk>
-     * @static
-     * @access public
+     *
      * @return array
      */
     public static function getMailinglistMessages()
     {
-        return DB::table('messages')->where('mailinglist', 1)->orderBy('created_at', 'DESC')->get();
+        return DB::table('messages')->where('mailinglist', 1)
+                  ->orderBy('created_at', 'DESC')->get();
     }
 
     /**
      * Get all public messages
-     * @author Javier Arias <ja573@cam.ac.uk>
-     * @static
-     * @access public
+     *
      * @return array
      */
     public static function getPublicMessages()
     {
-        return DB::table('messages')->where(['public' => 1, 'mailinglist' => 0])->orderBy('created_at', 'DESC')->get();
+        return DB::table('messages')
+                  ->where(['public' => 1, 'mailinglist' => 0])
+                  ->orderBy('created_at', 'DESC')->get();
     }
 }
+
