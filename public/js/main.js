8a4f5c7a57c717fe80d3e11ef95883ea1ba220f9
Javier Arias <javier@arias.re>
2017-09-05 14:29:24 +0100

    remove blank space

--- public/js/main.js
+++ public/js/main.js
@@ -7,19 +7,19 @@ function hide(id) {
 }
 
 function hideSendMail()
-{ 
+{
     if ($('#recipient-mailing').is(':checked')) {
         hide('mail-visibility');
         hide('mail-to');
-    } 
-    
+    }
+
     if($('#recipient-other').is(':checked')) {
         unhide('mail-visibility');
         unhide('mail-to');
     }
 }
 
-function getFileDetails(id) 
+function getFileDetails(id)
 {
     var selectedFile = document.getElementById(id).files[0];
     $('#'+id+'_details').html(selectedFile.name);
