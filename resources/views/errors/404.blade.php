a78b6c03d1fab6b7bd1a94fda77d5698c208a1e3
Javier Arias <javier@arias.re>
2017-09-25 09:28:38 +0100

    add custom 404 error

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Something went wrong</title>
+
+        <link href="{{ asset('css/main.css') }}?v=12" rel="stylesheet" type="text/css">
+
+        <style>
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="well line-break-top">
+                <div class="title">An error has occurred. Please contact <a href="mailto:info@fluids.ac.uk">info@fluids.ac.uk</a></div>
+            </div>
+        </div>
+    </body>
+</html>
