95f63f3e46ee135134d8aedaf81acce0bf2bd6aa
Javier Arias <javier@arias.re>
2017-09-28 11:11:21 +0100

    structure widget

--- resources/views/pages/tweets.blade.php
+++ resources/views/pages/tweets.blade.php
@@ -1,1 +1,8 @@
-<a class="twitter-timeline" data-height="1500" href="https://twitter.com/{{ $twitter }}">Tweets by {{ $twitter }}</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+@section('head')
+<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+@endsection
+
+<a class="twitter-timeline" data-height="1500"
+   href="https://twitter.com/{{ $twitter }}">
+    Tweets by {{ $twitter }}
+</a>
