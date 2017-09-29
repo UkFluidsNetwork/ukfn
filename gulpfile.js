ccd3c882b93d242475db7ec3d134e6c1ef3c5453
Javier Arias <javier@arias.re>
2017-09-29 11:28:07 +0100

    use gulp to copy vendor dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -14,5 +14,19 @@ var elixir = require('laravel-elixir');
 elixir(function(mix) {
     mix.copy('node_modules/bootstrap-sass/assets/javascripts/bootstrap.min.js',
              'public/js/bootstrap.min.js');
+    mix.copy('node_modules/jquery/dist/jquery.min.js',
+             'public/js/jquery.min.js');
+    mix.copy('node_modules/angular/angular.min.js',
+             'public/js/angular.min.js');
+    mix.copy('node_modules/angular-messages/angular-messages.min.js',
+             'public/js/angular-messages.min.js');
+    mix.copy('node_modules/ngstorage/ngStorage.min.js',
+             'public/js/ngStorage.min.js');
+    mix.copy('node_modules/ngmap/build/scripts/ng-map.min.js',
+             'public/js/ng-map.min.js');
+    mix.copy('node_modules/angular-selectize2/dist/selectize.js',
+             'public/js/selectize.js');
+    mix.copy('node_modules/angularjs-dropdown-multiselect/dist/angularjs-dropdown-multiselect.min.js',
+             'public/js/angularjs-dropdown-multiselect.min.js');
     mix.sass('main.scss');
 });
