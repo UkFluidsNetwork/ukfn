358d03df8735607db6f9ca6bdf1047996f63e4c1
Javier Arias <javier@arias.re>
2018-02-16 00:34:42 +0000

    add tutorial request

---
+++ app/Http/Requests/TutorialFormRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Http\Requests\Request;
+
+class TutorialFormRequest extends Request
+{
+
+    /**
+     * Determine if the user is authorized to make this request.
+     * We allow all requests as middleware is taking care of it.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|max:255',
+            'description' => 'required|max:255',
+            'author' => 'required|max:255',
+            'date' => 'required|date_format:"Y"',
+        ];
+    }
+}
+
