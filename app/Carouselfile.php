73663af26a069ca59959b9a34be7844764467bb4
Javier Arias <javier@arias.re>
2018-03-22 21:43:36 +0000

    add carousel model

---
+++ app/Carouselfile.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Carouselfile extends Model
+{
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+    ];
+
+    /**
+     * Get the files of the carousel
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\belongsTo
+     */
+    public function file()
+    {
+        return $this->belongsTo('App\File');
+    }
+}
