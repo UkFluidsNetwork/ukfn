dd7863bea6ada768fbb5b588d9e70b252e5fda7d
Javier Arias <javier@arias.re>
2018-07-13 12:08:13 +0100

    make aggregator optional for backwards display

--- resources/views/talks/view.blade.php
+++ resources/views/talks/view.blade.php
@@ -4,13 +4,13 @@
         <section class="line-break-dbl">
             <h3 class="text-danger line-break">{{ $talk->title }}</h3>
             <span class="talks-speaker display-block">{{ $talk->speaker }}</span>
-            <span class="display-block line-break">{{ $talk->aggregator->longname }}</span>
+            <span class="display-block line-break">{{ isset($talk->aggregator->longname) ? $talk->aggregator->longname : ""}}</span>
             <span class="text-muted display-block">{{ $talk->when }}</span>
             <span class="text-muted line-break-dbl display-block">{{ $talk->venue }}</span>
             <span class="line-break-dbl-top display-block line-breakdbl">{{ $talk->abstract }}</span>
 
             <div class="line-break-dbl-top">
-                
+
                 @if ($talk->displayRecording())
                     <div class="embed-responsive embed-responsive-16by9">
                         <iframe class="embed-responsive-item" src="{{ $talk->recordingurl }}" scrolling="no" frameborder="0" allowfullscreen></iframe>
