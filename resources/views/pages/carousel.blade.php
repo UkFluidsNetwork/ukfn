8530ed4e334d4efb263301e0b8d132b007599a0e
Javier Arias <javier@arias.re>
2018-07-30 15:09:24 +0100

    add new tab link

--- resources/views/pages/carousel.blade.php
+++ resources/views/pages/carousel.blade.php
@@ -9,6 +9,13 @@
     .carousel-caption {
         background-color:rgba(0, 0, 0, 0.3);
     }
+    .new-window {
+       color: inherit;
+    }
+    .new-window:hover,
+    .new-window:active {
+       color: grey;
+    }
 </style>
 
 <div id="carousel" class="carousel slide" data-ride="carousel"
@@ -28,7 +35,13 @@
       @endif
       <div class="container">
         <div class="carousel-caption">
-          <h3>{{ $item->name }}</h3>
+          <h3>
+              {{ $item->name }}
+              <a class="new-window" title="Open in a new tab" target="_blank" href="@if ($item->file->name == $item->file->path) {{ $item->file->name }} @else {{ $item->file->path }}/{{ $item->file->name }} @endif">
+                <span class="glyphicon glyphicon-new-window" aria-hidden="true">
+                </span>
+              </a>
+          </h3>
           <h4>{{ $item->author }}</h4>
           <p>{{ $item->description }}</p>
         </div>
